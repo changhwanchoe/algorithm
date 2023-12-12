fa42b58ae1abf9256358d9cfe857021160cb3289
CCH <126532936+changhwanchoe@users.noreply.github.com>
2023-12-12 11:20:02 +0900

    [Silver IV] Title: 등수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.cc
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#define endl '\n'
+using namespace std;
+
+int n, score, p, ans;
+vector<int> v;
+
+void init()
+{
+	ans=1;
+	cin>>n>>score>>p;
+	v.resize(n);
+	for(int i=0;i<n;i++)
+		cin>>v[i];
+}
+
+void run()
+{
+	int cnt=0;
+	for(int i=0;i<v.size();i++) {
+		if(v[i] > score)
+			ans++;
+		else if(v[i] == score);
+		else break;
+		cnt++;
+	}
+	if(n==0)
+		ans=1;
+	else if(p==cnt)
+		ans=-1;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr);
+	init();
+	run();
+	cout<<ans;
+	return 0;
+}

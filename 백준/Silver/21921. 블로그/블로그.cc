d82ff967efd849d0c46a90da441c0aba4eefbea8
CCH <126532936+changhwanchoe@users.noreply.github.com>
2023-12-15 06:50:21 +0900

    [Silver III] Title: 블로그, Time: 24 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.cc
+++ 백준/Silver/21921. 블로그/블로그.cc
@@ -2,46 +2,35 @@
 #define endl '\n'
 using namespace std;
 
-int n, ans, cnt;
-vector<int> v;
-unordered_map<int, int> m;
-
-void init()
-{
-	int temp;
-	cin>>temp>>n;
-	v.resize(temp);
-	for(int i=0;i<temp;i++)
-		cin>>v[i];
-}
-
 void run()
 {
-	int left, right, sum;
-	left=right=sum=0;
-	while(right<=v.size()) {
-		if(right-left < n)
-			sum+=v[right++];
-		else {
-			if(m.find(sum)==m.end())
-				m.insert({sum, 1});
-			else
-				m[sum]++;
-			ans=max(ans, sum);
-			sum-=v[left++];
-		}
+	int n, k, sum=0;
+	deque<int> dq;
+	cin>>n>>k;
+	n-=k;
+	while(k--) {
+		int x; cin>>x;
+		sum+=x; dq.push_back(x);
 	}
-	cnt=m[ans];
+	int mx=sum, cnt=1;
+	while(n--) {
+		int x; cin>>x;
+		sum+=x; dq.push_back(x);
+		sum-=dq.front(); dq.pop_front();
+		if(sum>mx) {
+			mx=sum;
+			cnt=1;
+		} else if(sum==mx) cnt++;
+	}
+	if(mx!=0)
+		cout<<mx<<endl<<cnt;
+	else
+		cout<<"SAD";
 }
 
 int main()
 {
 	ios::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr);
-	init();
 	run();
-	if(ans!=0)
-		cout<<ans<<endl<<cnt;
-	else
-		cout<<"SAD";
 	return 0;
 }
